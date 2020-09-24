f8d8493f32c9ae55ea7748158d540a274ce02e37
GITSALAHE <salah.bouanba2@gmail.com>
2020-09-24 21:00:14 +0100

    done AI

---
+++ main.py
@@ -0,0 +1,16 @@
+import cv2
+import sys
+image=sys.argv[1]
+
+face_cascade = cv2.CascadeClassifier('data/frontal.xml')
+
+img = cv2.imread(image)
+gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+
+faces = face_cascade.detectMultiScale(gray, 1.3, 4)
+
+for (x, y, w, h) in faces:
+    cv2.rectangle(img, (x, y), (x+w, y+h), (255, 0, 0), 2)
+
+cv2.imshow('face detector by GITSALAH using opencv', img)
+cv2.waitKey()
